f2294720460e9c2bc45eae19152c5f332f4e3302
cmartinavila <kinbrian5@hotmail.com>
2022-02-12 00:57:23 +0100

    Suppression de l'attribut component dans Entity.java

--- core/src/up/wargroove/core/character/Entity.java
+++ core/src/up/wargroove/core/character/Entity.java
@@ -16,43 +16,41 @@ public abstract class Entity {
 
     public static enum Type {
 
-        VILLAGER(Component.GROUND, Movement.Type.WALKING, 3),
-        COMMANDER(Component.GROUND, Movement.Type.WALKING, 4),
-        WAGON(Component.GROUND, Movement.Type.WHEELS, 12),
-        SPAREMAN(Component.GROUND, Movement.Type.WALKING, 3),
-        DOG(Component.GROUND, Movement.Type.WALKING, 5),
-        ARCHER(Component.GROUND, Movement.Type.WALKING, 3),
-        CAVALRY(Component.GROUND, Movement.Type.RIDING, 6),
-        SOLDIER(Component.GROUND, Movement.Type.WALKING, 4),
-        BALLISTA(Component.GROUND, Movement.Type.WHEELS, 6),
-        TREBUCHET(Component.GROUND, Movement.Type.WHEELS, 6),
-        GIANT(Component.GROUND, Movement.Type.WALKING, 5),
-        RIFLEMAN(Component.GROUND, Movement.Type.WALKING, 4),
-        THIEF(Component.SEA, Movement.Type.WALKING, 6),
+        VILLAGER(Movement.Type.WALKING, 3),
+        COMMANDER(Movement.Type.WALKING, 4),
+        WAGON(Movement.Type.WHEELS, 12),
+        SPAREMAN(Movement.Type.WALKING, 3),
+        DOG(Movement.Type.WALKING, 5),
+        ARCHER(Movement.Type.WALKING, 3),
+        CAVALRY(Movement.Type.RIDING, 6),
+        SOLDIER(Movement.Type.WALKING, 4),
+        BALLISTA(Movement.Type.WHEELS, 6),
+        TREBUCHET(Movement.Type.WHEELS, 6),
+        GIANT(Movement.Type.WALKING, 5),
+        RIFLEMAN(Movement.Type.WALKING, 4),
+        THIEF(Movement.Type.WALKING, 6),
 
-        BALLON(Component.AIR, Movement.Type.FLYING, 6),
-        AERONAUT(Component.AIR, Movement.Type.FLYING, 5),
-        SKY_RIDER(Component.AIR, Movement.Type.FLYING, 7),
-        DRAGON(Component.AIR, Movement.Type.FLYING, 8),
+        BALLON(Movement.Type.FLYING, 6),
+        AERONAUT(Movement.Type.FLYING, 5),
+        SKY_RIDER(Movement.Type.FLYING, 7),
+        DRAGON(Movement.Type.FLYING, 8),
 
-        BRAGE(Component.SEA, Movement.Type.WATER, 10),
-        AMPHIBIAN(Component.SEA, Movement.Type.AMPHIBIOUS, 5),
-        TURTLE(Component.SEA, Movement.Type.WATER, 12),
-        HARPOON_SHIP(Component.SEA, Movement.Type.WATER, 4),
-        WARSHIP(Component.SEA, Movement.Type.WATER, 8),
+        BRAGE(Movement.Type.WATER, 10),
+        AMPHIBIAN(Movement.Type.AMPHIBIOUS, 5),
+        TURTLE(Movement.Type.WATER, 12),
+        HARPOON_SHIP(Movement.Type.WATER, 4),
+        WARSHIP(Movement.Type.WATER, 8),
 
-        CRYSTAL(Component.GROUND, Movement.Type.NULL, 0),
-        VINE(Component.SEA, Movement.Type.AMPHIBIOUS, 5),
-        SPARROW_BOMB(Component.AIR, Movement.Type.HOVER, 5),
-        SHADOW_SISTER(Component.GROUND, Movement.Type.WALKING, 4);
+        CRYSTAL(Movement.Type.NULL, 0),
+        VINE(Movement.Type.AMPHIBIOUS, 5),
+        SPARROW_BOMB(Movement.Type.HOVER, 5),
+        SHADOW_SISTER(Movement.Type.WALKING, 4);
 
         public Movement.Type movement;
-        public Component component;
         public int movementCost;
 
-        Type(Component component, Movement.Type movement, int movementCost) {
+        Type(Movement.Type movement, int movementCost) {
 	        this.movement = movement;
-		    this.component = component;
             this.movementCost = movementCost;
 	    }
 
