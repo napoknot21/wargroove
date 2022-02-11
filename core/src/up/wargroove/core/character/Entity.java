10cc8cd89025a16d574a801fe86de1c8cf808867
cmartinavila <kinbrian5@hotmail.com>
2022-02-12 00:52:16 +0100

    Ajoute de mouvement et coût de mouvement pour les personnages

--- core/src/up/wargroove/core/character/Entity.java
+++ core/src/up/wargroove/core/character/Entity.java
@@ -16,45 +16,45 @@ public abstract class Entity {
 
     public static enum Type {
 
-        VILLAGER(Component.GROUND),
-        COMMANDER(Component.GROUND),
-        WAGON(Component.GROUND),
-        SPAREMAN(Component.GROUND),
-        DOG(Component.GROUND),
-        ARCHER(Component.GROUND),
-        CAVALRY(Component.GROUND),
-        SOLDIER(Component.GROUND),
-        BALLISTA(Component.GROUND),
-        TREBUCHET(Component.GROUND),
-        GIANT(Component.GROUND),
-        RIFLEMAN(Component.GROUND),
-        THIEF(Component.SEA),
+        VILLAGER(Component.GROUND, Movement.Type.WALKING, 3),
+        COMMANDER(Component.GROUND, Movement.Type.WALKING, 4),
+        WAGON(Component.GROUND, Movement.Type.WHEELS, 12),
+        SPAREMAN(Component.GROUND, Movement.Type.WALKING, 3),
+        DOG(Component.GROUND, Movement.Type.WALKING, 5),
+        ARCHER(Component.GROUND, Movement.Type.WALKING, 3),
+        CAVALRY(Component.GROUND, Movement.Type.RIDING, 6),
+        SOLDIER(Component.GROUND, Movement.Type.WALKING, 4),
+        BALLISTA(Component.GROUND, Movement.Type.WHEELS, 6),
+        TREBUCHET(Component.GROUND, Movement.Type.WHEELS, 6),
+        GIANT(Component.GROUND, Movement.Type.WALKING, 5),
+        RIFLEMAN(Component.GROUND, Movement.Type.WALKING, 4),
+        THIEF(Component.SEA, Movement.Type.WALKING, 6),
 
-        BALLON(Component.AIR),
-        AERONAUT(Component.AIR),
-        SKY_RIDER(Component.AIR),
-        DRAGON(Component.AIR),
+        BALLON(Component.AIR, Movement.Type.FLYING, 6),
+        AERONAUT(Component.AIR, Movement.Type.FLYING, 5),
+        SKY_RIDER(Component.AIR, Movement.Type.FLYING, 7),
+        DRAGON(Component.AIR, Movement.Type.FLYING, 8),
 
-        BRAGE(Component.SEA),
-        AMPHIBIAN(Component.SEA),
-        TURTLE(Component.SEA),
-        HARPOON_SHIP(Component.SEA),
-        WARSHIP(Component.SEA),
+        BRAGE(Component.SEA, Movement.Type.WATER, 10),
+        AMPHIBIAN(Component.SEA, Movement.Type.AMPHIBIOUS, 5),
+        TURTLE(Component.SEA, Movement.Type.WATER, 12),
+        HARPOON_SHIP(Component.SEA, Movement.Type.WATER, 4),
+        WARSHIP(Component.SEA, Movement.Type.WATER, 8),
 
-        CRYSTAL(Component.GROUND),
-        VINE(Component.SEA),
-        SPARROW_BOMB(Component.AIR),
-        SHADOW_SISTER(Component.GROUND);
+        CRYSTAL(Component.GROUND, Movement.Type.NULL, 0),
+        VINE(Component.SEA, Movement.Type.AMPHIBIOUS, 5),
+        SPARROW_BOMB(Component.AIR, Movement.Type.HOVER, 5),
+        SHADOW_SISTER(Component.GROUND, Movement.Type.WALKING, 4);
 
-	public Movement.Type movement;
-	public Component component;
-	public int cost = 5;
+        public Movement.Type movement;
+        public Component component;
+        public int movementCost;
 
-        Type(Component component) {
-	
-		this.component = component;
-
-	}
+        Type(Component component, Movement.Type movement, int movementCost) {
+	        this.movement = movement;
+		    this.component = component;
+            this.movementCost = movementCost;
+	    }
 
     };
 
@@ -65,8 +65,7 @@ public abstract class Entity {
 
         this.name = name;
     	this.type = type;
-
-	tmpCost = type.cost;
+        tmpCost = type.movementCost;
 
     }
 
