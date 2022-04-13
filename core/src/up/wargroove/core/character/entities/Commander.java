9ab466e1ecd47d8eb5a28f7979341de2f8747c72
napoknot21 <kinbrian5@hotmail.com>
2022-04-13 22:57:49 +0200

    commander.yml update et modularité des fonctions attack dans Commander.java

--- core/src/up/wargroove/core/character/entities/Commander.java
+++ core/src/up/wargroove/core/character/entities/Commander.java
@@ -1,6 +1,5 @@
 package up.wargroove.core.character.entities;
 
-
 import org.gradle.internal.impldep.org.yaml.snakeyaml.Yaml;
 import up.wargroove.core.character.Character;
 import up.wargroove.core.character.Entity;
@@ -8,11 +7,15 @@ import up.wargroove.core.character.Faction;
 import up.wargroove.core.character.Movement;
 import up.wargroove.utils.Pair;
 
+import javax.naming.ldap.HasControls;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InaccessibleObjectException;
+import java.net.CacheRequest;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
@@ -47,34 +50,88 @@ public class Commander extends Character {
 
     }
 
-/*
+	/*
+
+	private HashMap<String, Map<String, List<Integer>>> getMatrixValues () {
+		try {
+			File f = new File("./core/src/up/wargroove/core/character/damageMatrix/commander.yml");
+			InputStream is = new FileInputStream(f);
+			Yaml yml = new Yaml();
+			return yml.load((is));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public Map<String,List<Integer>> getAttacksDefendsValues (Character ch) {
+		var data = getMatrixValues();
+		if (data == null || ch == null) return null;
+		System.out.println("Hola1");
+		return data.get(ch.getClass().getName().toUpperCase());
+	}
+
+	//FIXME corriger pb de valeur de retour null
+	public Pair<Integer,Integer> getAttacksValues (Character ch) {
+		var data = getAttacksDefendsValues(ch);
+		System.out.println("Hola2");
+		if (data == null) return null;
+		System.out.println("Hola3");
+		List<Integer> attacks = data.get("attacks");
+		if (attacks.size() != 2) return null;
+		Pair<Integer,Integer> pairAttack = new Pair<>();
+		pairAttack.first = attacks.get(0);
+		pairAttack.second = attacks.get(1);
+		return pairAttack;
+	}
+
+	public Pair<Integer,Integer> getDefendsValues (Character ch) {
+		var data =  getAttacksDefendsValues(ch);
+		if (data == null) return null;
+		List<Integer> defends = data.get("defends");
+		if (defends.size() != 2) return null;
+		Pair<Integer,Integer> pairDefend = new Pair<>();
+		pairDefend.first = defends.get(0);
+		pairDefend.second = defends.get(1);
+		return pairDefend;
+	}
 
 	public void attack (Character ch) {
-		String name = this.getClass().getName();
-		name = name.toUpperCase();
-		try {
-			File f = new File("../damageMatrix/"+name+".yml");
-			InputStream is = new FileInputStream(f);
-
-			Yaml yml = new Yaml();
-
-			Map<String, Map<String, Pair <Integer,Integer>>> data = yml.load(is);
-
-			String name_attack = name.toLowerCase();
-			String name_defend = ch.getClass().getName().toLowerCase();
-
-			var actions = data.get(name_defend);
-
-			Pair<Integer,Integer> values_attack = actions.get("attacks");
-			Pair<Integer,Integer> values_defend = actions.get("defends");
-
+		//Normal values (premier integer de Pais)
+		Pair<Integer, Integer> values_attack = getAttacksValues(ch);
+		Pair<Integer, Integer> values_defend = getDefendsValues(ch);
+		if (values_attack == null && values_defend == null) {
+			values_attack = new Pair<>();
+			values_attack.first = 0;
+			values_attack.second = 0;
+			values_defend = new Pair<>();
+			values_defend.first = 0;
+			values_defend.second = 0;
+		} else if (values_attack == null) {
+			values_attack = new Pair<>();
+			values_attack.first = 0;
+			values_attack.second = 0;
+		} else if (values_defend == null) {
+			values_defend = new Pair<>();
+			values_defend.first = 0;
+			values_defend.second = 0;
+		} else {
 			ch.setHealth(ch.getHealth() - values_attack.first + values_defend.first);
-
-		} catch (Exception exception) {
-			exception.printStackTrace();
 		}
 
-	}
-*/
 
+
+	}
+
+	public static void main(String[] args) {
+		Commander cd = new Commander();
+		Villager vl = new Villager();
+		System.out.println("Hello world");
+		//System.out.println(cd.getClass().getName());
+		System.out.println(cd.getAttacksValues(vl).toString());
+
+	}
+
+
+	 */
 }
