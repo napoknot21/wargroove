a309c63ab75321841a2cabe636ecfb4d9da34fc6
garnierk <kevin_garnier@outlook.fr>
2022-04-28 17:01:10 +0200

    exportMap plugin improvement

--- plugins/src/main/java/up/wargroove/plugins/tasks/ExportMap.java
+++ plugins/src/main/java/up/wargroove/plugins/tasks/ExportMap.java
@@ -1,18 +1,14 @@
 package up.wargroove.plugins.tasks;
 
-import org.gradle.api.Project;
-import org.gradle.api.tasks.Input;
-import org.gradle.api.tasks.InputFile;
-import org.gradle.api.tasks.TaskAction;
-import org.gradle.api.tasks.options.Option;
-import up.wargroove.core.world.GeneratorProperties;
-import up.wargroove.core.world.World;
-import up.wargroove.core.world.WorldProperties;
+
+import up.wargroove.core.world.*;
+import up.wargroove.plugins.Reader;
 import up.wargroove.utils.Database;
 import up.wargroove.utils.DbObject;
 import up.wargroove.utils.Pair;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 
 /**
@@ -23,6 +19,11 @@ public class ExportMap {
      * Name of the map stored in the database.
      */
     private String name;
+
+    /**
+     * Name of the map stored in the database.
+     */
+    private String collection;
     /**
      * The path where the map will be exported.
      */
@@ -72,7 +73,6 @@ public class ExportMap {
      *
      * @throws Exception if an error occurred.
      */
-    @TaskAction
     public void run() throws Exception {
         if (name == null || name.isBlank() || path == null || path.isBlank()) {
             throw new Exception("The args cannot be empty");
@@ -117,17 +117,23 @@ public class ExportMap {
     }
 
 
-    @Option(option = "name", description = "name of the map stored in the database")
+    private String getWorldTexturePath() {
+        return "core/assets/data/sprites/world/grass.atlas";
+    }
+    //(option = "name", description = "name of the map stored in the database")
     private void setName(String name) {
-        this.name = name;
+        String[] s = name.split("/");
+        this.collection = s[0];
+        this.name = s[1];
+
     }
 
-    @Option(option = "path", description = "The path where the map will be exported")
+    //(option = "path", description = "The path where the map will be exported")
     private void setPath(String path) {
         this.path = path;
     }
 
-    @Option(option = "gen", description = "Select if the map will be generated before being exported (Y/N)")
+    //(option = "gen", description = "Select if the map will be generated before being exported (Y/N)")
     private void setGeneration(String generation) {
         gen = generation.equalsIgnoreCase("Y");
     }
@@ -137,7 +143,6 @@ public class ExportMap {
      *
      * @return the default dimension.
      */
-    @Input
     private Pair<Integer, Integer> getDimension() {
         return new Pair<>(20, 20);
     }
@@ -149,8 +154,8 @@ public class ExportMap {
      * @param ds The CLI argument.
      * @throws Exception the argument doesn't respect the format
      */
-    @Option(option = "dim", description = "World dimension. The syntaxe MUST be the following : "
-            + "dimension.first, dimension.second")
+    /*(option = "dim", description = "World dimension. The syntaxe MUST be the following : "
+            + "dimension.first, dimension.second")*/
     private void setDimension(String ds) throws Exception {
         String[] dim = ds.split(",");
         if (dim.length != 2) {
@@ -161,44 +166,43 @@ public class ExportMap {
         dimension = new Pair<>(first, second);
     }
 
-    @Option(option = "r", description = "Generations parameters repartition")
+    //(option = "r", description = "Generations parameters repartition")
     private void setRepartition(String repartition) {
         this.repartition = Integer.parseInt(repartition);
     }
 
-    @Option(option = "overwrite", description = "indicate if a file with the same name as the one given "
-            + "in the path or the default one will be be overwritten (Y/N)")
+    /*(option = "overwrite", description = "indicate if a file with the same name as the one given "
+            + "in the path or the default one will be be overwritten (Y/N)")*/
     private void setOverwrite(String overwrite) {
         this.overwrite = overwrite.equalsIgnoreCase("Y");
     }
 
-    @Option(option = "n", description = "Generations parameters normalization")
+    //(option = "n", description = "Generations parameters normalization")
     private void setNormalization(String normalization) {
         this.normalization = Double.parseDouble(normalization);
     }
 
-    @Option(option = "s", description = "Generations parameters smooth")
+    //(option = "s", description = "Generations parameters smooth")
     private void setSmooth(String smooth) {
         this.smooth = Double.parseDouble(smooth);
     }
 
-    @InputFile
     private File getRoot() {
         return new File("db/wargroove.db");
     }
 
-    @Input
     private String getExtension() {
         return ".csv";
     }
 
     // TODO: 10/03/2022 : Improvement of enum gestion
     private void write(File file, WorldProperties properties) throws Exception {
+
+        int[][] tiles = readTerrain(properties);
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < dimension.first; i++) {
-            for (int j = 0; j < dimension.second; j++) {
-                int key = properties.terrain[i * dimension.first + j].getType().ordinal();
-                builder.append(key).append(";");
+        for (int i = tiles.length - 1; i>= 0; i--) {
+            for (int id : tiles[i]) {
+                builder.append(id).append(";");
             }
             builder.append('\n');
         }
@@ -206,6 +210,39 @@ public class ExportMap {
         writer.write(builder.toString());
         writer.flush();
         writer.close();
+    }
+
+    private int[][] readTerrain(WorldProperties properties) throws FileNotFoundException {
+        int[][] array = new int[properties.dimension.first][properties.dimension.second];
+        Reader reader = new Reader(getWorldTexturePath());
+        reader.load();
+        for (int i = array.length - 1; i >= 0 ; i--) {
+            for (int j = 0; j < array[i].length ; j++) {
+                Tile tile = properties.terrain[i * dimension.first + j];
+                array[i][j] = getTileId(reader,tile);
+            }
+        }
+        return array;
+    }
+
+    private int getTileId(Reader reader, Tile tile) {
+        String s;
+        if (tile.entity.isEmpty()) {
+            s = (tile.getType().toString());
+        }
+        else {
+
+            if (tile.entity.get() instanceof Stronghold || tile.entity.get() instanceof Village) {
+                s = String.valueOf(((Structure) tile.entity.get()).getStructureType());
+            } else {
+                s = ((Recruitment)tile.entity.get()).getRecruitmentType().toString();
+            }
+
+            s += "-" + tile.entity.get().getFaction();
+        }
+
+        s += "-" +tile.getTextureVersion();
+        return reader.indexOf(s);
     }
 
     private File createDestination(File destination) throws Exception {
@@ -232,7 +269,7 @@ public class ExportMap {
 
     private WorldProperties getWorld() throws Exception {
         Database data = new Database(getRoot());
-        data.selectCollection("worlds");
+        data.selectCollection(collection);
         DbObject object = data.get(name);
         if (object == null) {
             throw new Exception("The specified map doesn't exist");
