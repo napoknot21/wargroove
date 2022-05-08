1e089d47e60234a55b03b3e858f1581ad539b242
garnierk <kevin_garnier@outlook.fr>
2022-05-08 20:16:47 +0200

    add health ratio on damage

--- core/src/up/wargroove/core/character/Entity.java
+++ core/src/up/wargroove/core/character/Entity.java
@@ -3,30 +3,25 @@ package up.wargroove.core.character;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Null;
+import org.gradle.internal.impldep.org.yaml.snakeyaml.Yaml;
 import up.wargroove.core.world.Structure;
 import up.wargroove.utils.Constants;
 import up.wargroove.utils.DbObject;
-import up.wargroove.utils.Savable;
-import org.gradle.internal.impldep.org.yaml.snakeyaml.Yaml;
-import up.wargroove.utils.Constants;
 import up.wargroove.utils.Pair;
+import up.wargroove.utils.Savable;
 
-import java.util.*;
-import java.io.File;
 import java.io.FileInputStream;
+import java.util.*;
 
 public abstract class Entity implements Savable {
 
+    private static final Random r = new Random();
     protected Type type;
-
-    private final Random r = new Random();
-
     protected Movement movement;
-    private Faction faction;
-    private double health;
-
     protected int movRange;
     protected boolean isExhausted;
+    private Faction faction;
+    private double health;
 
     /**
      * constructeur pour Entity
@@ -42,15 +37,27 @@ public abstract class Entity implements Savable {
 
     }
 
-    private Entity () {
+    private Entity() {
         this(Type.STRUCTURE, Faction.OUTLAWS);
     }
 
-    public void setHealth(double health) {
-        if (health < 0) this.health = 0;
-        if (health > 100) this.health = 100;
-
-        else this.health = health;
+    public static Entity loadEntity(DbObject from) {
+        if (from == null || from.isEmpty()) {
+            return null;
+        }
+        Type type = Type.valueOf(from.get(Constants.ENTITY_TYPE_DB_KEY).get());
+        Faction faction = Faction.valueOf(from.get(Constants.ENTITY_FACTION_DB_KEY).get());
+        Entity e;
+        if (type == Type.STRUCTURE) {
+            e = Structure.loadStructure(from, faction);
+        } else {
+            e = EntityManager.instantiate(type, "", faction);
+        }
+        if (e == null) {
+            return null;
+        }
+        e.setHealth(Double.parseDouble(from.get(Constants.ENTITY_HEALTH_DB_KEY).get()));
+        return e;
     }
 
     public void initialize() {
@@ -60,7 +67,7 @@ public abstract class Entity implements Savable {
 
     public boolean isExhausted() {
 
-	    return isExhausted;
+        return isExhausted;
 
     }
 
@@ -107,23 +114,10 @@ public abstract class Entity implements Savable {
         return health;
     }
 
-    public static Entity loadEntity(DbObject from) {
-        if (from == null || from.isEmpty()) {
-            return null;
-        }
-        Type type = Type.valueOf(from.get(Constants.ENTITY_TYPE_DB_KEY).get());
-        Faction faction = Faction.valueOf(from.get(Constants.ENTITY_FACTION_DB_KEY).get());
-        Entity e;
-        if(type == Type.STRUCTURE) {
-            e = Structure.loadStructure(from, faction);
-        } else {
-            e = EntityManager.instantiate(type, "", faction);
-        }
-        if (e == null) {
-            return null;
-        }
-        e.setHealth(Double.parseDouble(from.get(Constants.ENTITY_HEALTH_DB_KEY).get()));
-        return e;
+    public void setHealth(double health) {
+        if (health < 0) this.health = 0;
+        else if (health > 100) this.health = 100;
+        else this.health = health;
     }
 
     @Override
@@ -131,7 +125,7 @@ public abstract class Entity implements Savable {
         DbObject res = new DbObject();
         res.put(Constants.ENTITY_HEALTH_DB_KEY, health);
         res.put(Constants.ENTITY_FACTION_DB_KEY, faction.toString());
-        res.put(Constants.ENTITY_TYPE_DB_KEY,type.toString());
+        res.put(Constants.ENTITY_TYPE_DB_KEY, type.toString());
         return res;
     }
 
@@ -139,23 +133,28 @@ public abstract class Entity implements Savable {
     public void load(DbObject from) {
     }
 
-    public void attack (Entity ch) {
+    public void attack(Entity ch) {
         if (ch == null) return;
-        Pair<Integer,Integer> attacksMedian = this.getAttacksValues(ch);
-        int baseDamage = this.getAttacksAndBaseValues(ch).get("base").get(0);
-        int a = attacksMedian.first - baseDamage;
+        double healthRatio = Math.max(0.1, getHealth() / 100);
+        Pair<Integer, Integer> attacksMedian = this.getAttacksValues(ch);
+        if (r.nextInt(10) == 0) {
+            ch.setHealth(ch.getHealth() - attacksMedian.second * healthRatio);
+        }
+        int attack = (int) (attacksMedian.first * healthRatio);
+        int baseDamage = (int) (this.getAttacksAndBaseValues(ch).get("base").get(0) * healthRatio);
+        int a = attack - baseDamage;
         if (a <= 0) {
-            ch.setHealth((ch.getHealth() - baseDamage < 0) ? 0 : (ch.getHealth() - baseDamage) );
+            ch.setHealth(ch.getHealth() - baseDamage);
             return;
         }
-        int min = attacksMedian.first - a;
-        int max = attacksMedian.first + a;
+        int min = attack - a;
+        int max = attack + a;
         int randAttack = r.nextInt(max - min + 1) + min;
-        ch.setHealth((ch.getHealth() - randAttack < 0) ? 0 : (ch.getHealth() - randAttack) );
+        ch.setHealth(ch.getHealth() - randAttack);
 
     }
 
-    protected Map<String, Map<String, List<Integer>>> readDamageMatrixValues () {
+    protected Map<String, Map<String, List<Integer>>> readDamageMatrixValues() {
         try {
             String name = this.getType().toString().toLowerCase();
             FileHandle f = Gdx.files.internal(Constants.DEFAULT_DM_ROOT + name + ".yml");
@@ -167,26 +166,26 @@ public abstract class Entity implements Savable {
     }
 
 
-    public Map<String, List <Integer> > getAttacksAndBaseValues (Entity ch) {
+    public Map<String, List<Integer>> getAttacksAndBaseValues(Entity ch) {
         var data = this.readDamageMatrixValues();
         if (data == null || ch == null) return null;
         return data.get(ch.getType().name().toUpperCase());
     }
 
-    public Pair<Integer, Integer> getAttacksValues (Entity ch) {
+    public Pair<Integer, Integer> getAttacksValues(Entity ch) {
         var data = this.getAttacksAndBaseValues(ch);
-        if (data == null || ch == null) return new Pair<>(20,20);
-        List <Integer> attacks = data.get("attacks");
-        if (attacks.size() != 2) return new Pair<>(0,0);
-        return new Pair<>(attacks.get(0),attacks.get(1));
+        if (data == null || ch == null) return new Pair<>(20, 20);
+        List<Integer> attacks = data.get("attacks");
+        if (attacks.size() != 2) return new Pair<>(0, 0);
+        return new Pair<>(attacks.get(0), attacks.get(1));
     }
 
     @Null
-    public Collection<Type> getStrongAgainstValues(){
+    public Collection<Type> getStrongAgainstValues() {
         Map<String, Map<String, List<Integer>>> data = readDamageMatrixValues();
         if (data == null) return null;
         Collection<Type> c = new LinkedList<>();
-        data.forEach((n,d) -> {
+        data.forEach((n, d) -> {
             if (d.get("attacks").get(0) > 70) c.add(Type.valueOf(n.toUpperCase()));
         });
         return c;
@@ -233,7 +232,7 @@ public abstract class Entity implements Savable {
         TURTLE,
         WARSHIP,
         AMPHIBIAN,
-        BRAGE,
+        BARGE,
 
         CRYSTAL,
         VINE,
@@ -241,15 +240,15 @@ public abstract class Entity implements Savable {
         SHADOW_SISTER;
         */
 
-	    STRUCTURE;
+        STRUCTURE;
 
         public String prettyName() {
-                String[] name = name().split("_");
-                String ret = name[0].charAt(0) + name[0].substring(1).toLowerCase();
-                if (name.length == 2) {
-                    ret += " " + name[1].charAt(0) + name[1].substring(1).toLowerCase();
-                }
-                return ret;
+            String[] name = name().split("_");
+            String ret = name[0].charAt(0) + name[0].substring(1).toLowerCase();
+            if (name.length == 2) {
+                ret += " " + name[1].charAt(0) + name[1].substring(1).toLowerCase();
+            }
+            return ret;
         }
     }
 }
